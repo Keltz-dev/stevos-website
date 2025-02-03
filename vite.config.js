8fd61080e4526e6e0cb9d5007757dc36107b92a4
Karl von der Eltz <karlvondereltz@gmail.com>
2025-02-03 18:22:20 +0100

    let's see

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/stevos-website/", // Set to your repo name
+  base: "/", // Set to your repo name
 });
