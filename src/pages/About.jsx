81e2647ec377b07ccaea5317162559cd9d85e0d0
Karl von der Eltz <karlvondereltz@gmail.com>
2025-02-13 16:48:47 +0100

    changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,13 @@ const About = () => {
   return (
     <>
       <div className="container">
+        <div className="centered">
+          <img
+            className='rounded'
+            src="https://static.wixstatic.com/media/122352_6504cf07bea5441f8bc7288a67fa6384~mv2.jpg/v1/crop/x_0,y_598,w_2433,h_2433/fill/w_320,h_320,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/R1-03527-0030_JPG.jpg"
+            alt="Image of Steffen Sasse"
+          />
+        </div>
         <div className="grid even-rows">
           <div className="grid-item-cv">
             <div className="text-wrapper wrapper-cv">
